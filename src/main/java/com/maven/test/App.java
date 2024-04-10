367f70405cf49abde64ceff924196c976862cefb
Prakash <prakashbelieve@gmail.com>
2024-04-10 11:12:13 +0530

    First Commit

---
+++ src/main/java/com/maven/test/App.java
@@ -0,0 +1,15 @@
+package com.maven.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+	int a =10;
+        System.out.println( "Hello World!" );
+	System.out.println( "Commiting to git!" );
+    }
+}
