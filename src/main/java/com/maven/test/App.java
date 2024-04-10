a353371ba4ac9d4bdd3f0f212862b72fac0c1637
Prakash <prakashbelieve@gmail.com>
2024-04-10 11:14:57 +0530

    second push

--- src/main/java/com/maven/test/App.java
+++ src/main/java/com/maven/test/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
 	int a =10;
+	int c =a;
         System.out.println( "Hello World!" );
 	System.out.println( "Commiting to git!" );
     }
